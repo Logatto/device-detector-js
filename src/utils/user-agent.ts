b73749a6d68c4de6ecd2d5a4117f6968d36c8444
Wbeimar Logatto <wblomo22@gmail.com>
2019-05-28 15:00:03 -0500

    Revert "[Fix] crash in safari mac"

--- src/utils/user-agent.ts
+++ src/utils/user-agent.ts
@@ -1,9 +1,6 @@
 export const userAgentParser = (rawRegex: string, userAgent: string): string[] | null => {
-  try {
-    const regex = `(?:^|[^A-Z0-9-_]|[^A-Z0-9-]_|sprd-)(?:${rawRegex})`;
-    const match = RegExp(regex, "i").exec(userAgent);
-    return match ? match.slice(1) : null;
-  } catch (error) {
-    return null;
-  }
+  const regex = `(?:^|[^A-Z0-9-_]|[^A-Z0-9-]_|sprd-)(?:${rawRegex})`;
+  const match = RegExp(regex, "i").exec(userAgent);
+
+  return match ? match.slice(1) : null;
 };
